b59cfffa156427135c74343e4f6329aa9340b03c
Koushi Okamura <k.okamura@willdo.co.jp>
2014-04-15 11:18:01 +0900

    Update sql

--- sql/InsertData_Test.sql
+++ sql/InsertData_Test.sql
@@ -1,26 +1,26 @@
 insert into ToDoGrp_Tbl values(
-'プライベート',
+'プライベート'
 );
 insert into ToDoGrp_Tbl values(
-'仕事１',
+'仕事１'
 );
 
 insert into ToDoList_Tbl values(
 '',
-'プライベート'
+'プライベート',
 'お休み',
-'2014-4-19 10:00:00',
+'2014-4-19 10:00:00'
 );
 insert into ToDoList_Tbl values(
 '',
-'仕事１'
+'仕事１',
 '画面作成',
-'2014-4-16 10:00:00',
+'2014-4-16 10:00:00'
 );
 
 insert into ToDoList_Tbl values(
 '',
 '仕事１',
 'test',
-'2014-4-21 10:00:00',
+'2014-4-21 10:00:00'
 );
